f63382e5d43273aa370e3c8593a0bf97bbb5b1e6
David Linca <alexlinca_21@yahoo.ro>
2024-12-21 13:12:24 +0200

    Add project files.

---
+++ Linca_David_Lab11/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Linca_David_Lab11;
+using Linca_David_Lab11.Data;
+
+    public partial class App : Application
+{
+    public static ShoppingListDatabase Database { get; private set; }
+    public App()
+    {
+        Database = new ShoppingListDatabase(new RestService());
+        MainPage = new NavigationPage(new ListEntryPage());
+    }
+}
---
+++ Linca_David_Lab11/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Linca_David_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
